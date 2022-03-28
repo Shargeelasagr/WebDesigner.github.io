b901c5b46dd289a213c71ea3c1a424d3d2d69447
Shargeelasagr <shargeelkazam@gmail.com>
2022-03-28 17:00:45 +0500

    programer

---
+++ form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_post['name'];
+$visitor_email = $_post['email'];
+$subject = $_post['subject'];
+$message = $_post['message'];
+
+$email_from = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name./n".
+              "User Email: $visitor_email./n".
+              "Subject : $subject ./n".
+              "User Message: $message ./n";
+
+$to = 'shargeelkazam@gmail.com';
+
+$headers = "From : $email_from /r/n";
+         
+$headers .= "Reply_To $visitor_email /r/n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
